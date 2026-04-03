ad54a51ea41375530e45878cfc521f078eba40f4
Esha Eman <eshaarid5@gmail.com>
2026-04-03 16:40:06 +0500

    Fix dynamic port binding

--- backend/src/index.js
+++ backend/src/index.js
@@ -103,8 +103,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
  
